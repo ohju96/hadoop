6da312f96925b0a82bc0c1b3806abfdea3d4b8a9
ohju96 <ohju96@naver.com>
2022-04-26 13:37:06 +0900

    프로젝트 셋팅

---
+++ MapReduce/src/main/java/hadoop/MapReduce/MapReduceApplication.java
@@ -0,0 +1,13 @@
+package hadoop.MapReduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapReduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapReduceApplication.class, args);
+	}
+
+}
---
+++ MapReduce/src/test/java/hadoop/MapReduce/MapReduceApplicationTests.java
@@ -0,0 +1,13 @@
+package hadoop.MapReduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapReduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
