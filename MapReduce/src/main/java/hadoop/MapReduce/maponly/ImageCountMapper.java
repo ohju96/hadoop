036c25187a273588fd50604ae09f813ec3bddf0c
ohju96 <ohju96@naver.com>
2022-05-17 13:38:11 +0900

    맵리듀스를 사용하지 않는 맵 프로그래밍

---
+++ MapReduce/src/main/java/hadoop/MapReduce/maponly/ImageCountMapper.java
@@ -0,0 +1,38 @@
+package hadoop.MapReduce.maponly;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class ImageCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        String[] fields = value.toString().split("\"");
+
+        if (fields.length > 1) {
+            String request = fields[1];
+            fields = request.split(" ");
+
+            if (fields.length > 1) {
+
+                String fileName = fields[1].toLowerCase();
+
+                if (fileName.endsWith(".jpg")) {
+                    context.getCounter("imageCount", "jpg").increment(1);
+                } else if (fileName.endsWith(".gif")) {
+                    context.getCounter("imageCount", "gif").increment(1);
+                } else {
+                    context.getCounter("imageCount", "other").increment(1);
+                }
+            }
+        }
+
+    }
+}
